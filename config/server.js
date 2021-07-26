e4ce163067afd9efae6746c32415f1f85a0acae0
Hugues BUREAU <h.bureau@uwizy.com>
2021-07-26 15:58:48 +0200

    Initial codebase

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd77573289f4c7538703195c852406a7c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cbdd9197-9455-4b4b-b98d-e064b3b33d18'
+};
